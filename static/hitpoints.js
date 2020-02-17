7818be2e0cd94514aaa588612ee701b233b61622
pjsier <pjsier@gmail.com>
2020-02-16 18:06:09 -0600

    refactor: use var in script

--- static/hitpoints.js
+++ static/hitpoints.js
@@ -1,6 +1,6 @@
 (function() {
   if (!window.hitpoints_tracked) {
-    img = document.createElement("img");
+    var img = document.createElement("img");
     img.setAttribute("src", "%s://%s/pixel.gif?url=" + window.location.href);
     img.setAttribute("width", "1");
     img.setAttribute("height", "1");
